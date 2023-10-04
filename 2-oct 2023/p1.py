e1cd073b62a91671d54597f01c4d58c4ee1a4bfa
Afreena-Mohammad <137595442+Afreena-Mohammad@users.noreply.github.com>
2023-10-04 07:10:31 +0530

    Add files via upload

---
+++ 2-oct 2023/p1.py
@@ -0,0 +1,27 @@
+#functions
+'''def sum_of_numbers(n):
+    sum=0
+    for i in range(1,n+1):
+        sum=sum+i
+    return sum
+n=int(input())
+print(sum_of_numbers(n))'''
+'''def name(m):
+    return m
+n=input()
+print(name(n))'''
+'''def add(n):
+    return n
+m=input()
+print("Hello"+ " I am "+ add(m))'''
+n=input()
+if ((n.isdigit()  or len(n)==10) and(n[0]==6 or n[0]==7 or  n[0]==8 or  n[0]==9)):
+    print("it is phone number")
+else:
+    print("it is not phone number")
+    
+            
+            
+            
+        
+        
